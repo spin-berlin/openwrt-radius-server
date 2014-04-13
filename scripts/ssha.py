86ffd98d85fd205bb2822b789f242099c5cf7622
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-04-13 12:18:20 +0200

    check input; refine output

--- scripts/ssha.py
+++ scripts/ssha.py
@@ -7,6 +7,10 @@ import base64
 import os
 import sys
 
+if len(sys.argv) < 2:
+    print("Usage: ssha username")
+    sys.exit(1)
+
 username = sys.argv[1]
 
 password = base64.b64encode(os.urandom(32)) # base64 so we have a sane input
@@ -17,9 +21,11 @@ hash.update(password)
 hash.update(salt)
 
 # concat digest and salt and base64 encode to get the password hash for the database
-print(username + "\tSSHA-Password := \""
+print("Configuration: " + username + "\tSSHA-Password := \""
         + base64.b64encode(hash.digest() + salt).decode("utf-8")
         + "\""
     )
 
 print("User password: " + password.decode("utf-8"))
+
+sys.exit(0)
