a253e43cc6cc79f4ea5692ef476a8da4bfc90738
Philipp Borgers <borgers@mi.fu-berlin.de>
2014-04-13 12:11:22 +0200

    add script for password generation and hashing

---
+++ scripts/ssha.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# generates ssha1 password hashs for freeradius2
+
+import hashlib
+import base64
+import os
+import sys
+
+username = sys.argv[1]
+
+password = base64.b64encode(os.urandom(32)) # base64 so we have a sane input
+salt = os.urandom(32)
+
+hash = hashlib.sha1()
+hash.update(password)
+hash.update(salt)
+
+# concat digest and salt and base64 encode to get the password hash for the database
+print(username + "\tSSHA-Password := \""
+        + base64.b64encode(hash.digest() + salt).decode("utf-8")
+        + "\""
+    )
+
+print("User password: " + password.decode("utf-8"))
